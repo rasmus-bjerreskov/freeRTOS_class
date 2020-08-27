9ad822307d7736d0e3144eedcb1f7f974771ef0b
rasmus-bjerreskov <44022876+rasmus-bjerreskov@users.noreply.github.com>
2020-08-27 18:49:18 +0300

    dummy file for git testing

---
+++ test/src/testytest.cpp
@@ -0,0 +1,18 @@
+/*
+ * testytest.cpp
+ *
+ *  Created on: 27 Aug 2020
+ *      Author: Rasmus
+ */
+
+#include "testytest.h"
+
+testytest::testytest() {
+	// TODO Auto-generated constructor stub
+
+}
+
+testytest::~testytest() {
+	// TODO Auto-generated destructor stub
+}
+
